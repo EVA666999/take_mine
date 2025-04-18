5696e3ea5fca567c4a67ff4b83659f74f98ae100
EVA666999 <ukratitelkisok9913@inbox.ru>
2025-04-18 16:19:24 +0300

    dasdas

--- take_mine/app/urls.py
+++ take_mine/app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+namespace = 'app'
+
 urlpatterns = [
     path('', views.index, name='home'),
     path('category/create/', views.create_category, name='create_category'),
