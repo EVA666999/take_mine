20b530288a05249bf03024ee85f21d5c122e26db
EVA666999 <ukratitelkisok9913@inbox.ru>
2025-04-18 15:10:14 +0300

    dasdas

--- take_mine/app/views.py
+++ take_mine/app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+from take_mine.app.models import Item
+
+def home(request):
+    return render(request, 'app/home.html', {'items': Item.objects.all()})
