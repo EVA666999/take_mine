63edfd878e97b24f2946ce6d45a3f3b3939830ab
EVA666999 <ukratitelkisok9913@inbox.ru>
2025-04-24 14:24:35 +0300

    fix

--- take_mine/users/views.py
+++ take_mine/users/views.py
@@ -1,28 +1,28 @@
+from django.contrib.auth import get_user_model, login
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
 
-from app.models import User
 from app.utils import get_page_context
-
 from .forms import UserRegisterForm
 
+User = get_user_model()  # Правильный способ получения модели пользователя
 
 def register(request):
     if request.method == "POST":
         form = UserRegisterForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
             username = form.cleaned_data.get("username")
+            # Явный вход пользователя после регистрации
+            login(request, user)
             messages.success(
                 request, f"Аккаунт создан для {username}! Теперь вы можете войти."
             )
-            return redirect("users:login")
+            return redirect("app:index")
     else:
         form = UserRegisterForm()
     return render(request, "users/register.html", {"form": form})
-
-
 @login_required
 def profile(request, username):
     user = get_object_or_404(User, username=username)
